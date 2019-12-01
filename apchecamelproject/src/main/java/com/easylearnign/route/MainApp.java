7e5cd170e41653553c956ba86b070c82a34f511f
tharindu <tharindu@dreamsquadgroup.com>
2019-12-01 19:44:29 +0530

    camel project with simple file copy function

---
+++ apchecamelproject/src/main/java/com/easylearnign/route/MainApp.java
@@ -0,0 +1,21 @@
+package com.easylearnign.route;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        SimpleRouteBuilder routeBuilder = new SimpleRouteBuilder();
+        CamelContext ctx = new DefaultCamelContext();
+        try {
+            ctx.addRoutes(routeBuilder);
+            ctx.start();
+            Thread.sleep(5 * 60 * 1000);
+            ctx.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
